b69a05ac00c26d03ed4972dcc285fa13cf919aba
demetre10 <demetre.gotua@yahoo.com>
2024-10-27 17:20:34 +0400

    first commit

---
+++ day 3/claswork/lvl3.py
@@ -0,0 +1,21 @@
+name = "demetre"
+
+surname = "gotua"
+
+age = "10"
+
+pet = "turtle"
+
+print(name + " " + surname + " " + age + " " + pet)
+
+
+
+
+
+print(5+5)
+
+print(10-5)
+
+print(5 * 5)
+
+print(50/2)
---
+++ day 3/homework/lvl3.py
@@ -0,0 +1,46 @@
+name = "demetre"
+
+surname = "gotua"
+
+age = "10"
+
+hobi = "swimming"
+
+contri = "Georgia"
+
+print(name)
+
+print(surname)
+
+print(age)
+
+print(hobi)
+
+print(contri)
+
+
+print( name + " " + surname + " " + age + " " + hobi + " " + contri)
+
+print(15*2)
+
+print(20-10)
+
+print(40/2)
+
+print(25+5)
+
+print(18/2)
+
+print(15*2)
+
+print(36+17)
+
+print(15-10)
+
+print(78-64)
+
+print(90/3)
+
+print(10*5)
+
+print(25-24)
